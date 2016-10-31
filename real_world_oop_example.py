9a90bdfe5d00bb24839785d3714fa8fcc83b21d1
Stephen Oyaro <vystoya@gmail.com>
2016-10-31 17:04:43 +0300

    Initial commit

---
+++ real_world_oop_example.py
@@ -0,0 +1,13 @@
+class pet:
+  number_of_legs = 0
+  cat = pet()
+  doug.number_of_legs = 4
+print "Doug has %s legs." % doug.number_of_legs 
+
+def sleep(self):
+   print "purrr"
+    
+ cat = pet()
+ cat.sleep()
+def count_legs(self)
+print "I have %s legs" % self.number_of_legs
